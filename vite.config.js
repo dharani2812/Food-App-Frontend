f0fdc63139d6edfc3bada09d8d35dec1d35e7bea
Dharani Dharan <dharani@example.com>
2025-07-23 20:40:50 +0530

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/Food-App-Frontend/', // ✅ ADD THIS LINE
-  plugins: [react()],
-})
