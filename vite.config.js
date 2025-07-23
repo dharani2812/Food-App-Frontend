8d10f50930b98decb2277fe98eecac3bb678c3c3
Dharani Dharan <dharani@example.com>
2025-07-23 19:22:43 +0530

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Food-App-Frontend/', // 👈 Add this line
   plugins: [react()],
 })
+
